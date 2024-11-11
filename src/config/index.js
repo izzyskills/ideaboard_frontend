3bfc1ba632f25770aaaef6f6c70f514ff437176d
Israel Omola <izzyskills@gmail.com>
2024-11-11 18:26:11 +0100

    env

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const config = {
-  API_URL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
+  API_URL: import.meta.env.VITE_API_URL,
   IS_DEMO: Boolean(import.meta.env.VITE_IS_DEMO),
 };
